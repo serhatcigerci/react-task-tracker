8cb93541d8245021179e8f88f4770dd1a277f55e
serhatcigerci <yserhatcigerci@gmail.com>
2022-06-12 23:56:12 +0300

    .map list added

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -21,7 +21,11 @@ const tasks = [
 ]
 const Tasks = () => {
   return (
-    <div>Tasks</div>
+    <>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
   )
 }
 
