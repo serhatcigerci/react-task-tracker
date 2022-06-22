d29d5a460de6ec9cba8276849fd5abf36629fcf5
serhatcigerci <yserhatcigerci@gmail.com>
2022-06-22 21:20:36 +0300

    Add task added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -36,6 +37,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <AddTask />
       {tasks.length > 0 ? (<Tasks tasks = {tasks} onDelete={deleteTask} onToggle={toggleReminder} />) :
       (
         'No tasks to show'
