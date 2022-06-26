abc822eaa29592f3c3de608de6efb4f2bbb5100b
serhatcigerci <yserhatcigerci@gmail.com>
2022-06-26 21:11:14 +0300

    Toggle button added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,11 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    
-  }
+const Header = ({ title, onAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick}/>
+      <Button color='green' text='Add' onClick={onAdd}/>
     </header>
   )
 }
